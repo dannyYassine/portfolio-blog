c36a0caa70fcd3e187412e763251598b1ffeb6e0
Danny Yassine <dannyyassine@gmail.com>
2017-10-21 16:51:35 -0400

    no message

--- src/server/server.js
+++ src/server/server.js
@@ -7,8 +7,6 @@ const app = require('./app.js');
 require('./config/routes')(app);
 require('./config/env')(app);
 
-app.get
-
 const server = app.listen(app.get('port'), '0.0.0.0', function () {
     console.log("*\n*");
     console.log("/****************************************/");
